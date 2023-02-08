501acbf7433b45b7299e6b8f8f771301d35d01f4
ic59 <ic59@fresnou.org>
2023-02-08 10:23:39 -0800

    initial commit

---
+++ InputsNotes.js
@@ -0,0 +1,32 @@
+const readline = require('readline-sync');
+
+let answer = readline.question("What day is it?\nYour answer: ");
+console.log(answer);
+
+let username = readline.question("Enter username: ");
+let password = readline.question("Enter password: ");
+if (password == "abc"){
+    console.log(`Welcome ${username}`);
+} else {
+    console.log("invalid");
+}
+
+let currentPrice = readline.question("Enter current price: ");
+let originalPrice = readline.question("Enter original price: ");
+let diff = originalPrice-currentPrice;
+let discount = (diff/originalPrice).toFixed(2);
+console.log(`discount: ${discount}`);
+
+//Exponents
+let base = Number(readline.question("Enter the base: "));
+let exp = Number(readline.question("Enter the exponent: "));
+let answer2 = base ** exp
+console.log(`${base} to the power of ${exp} = ${answer2}`);
+
+//rounding
+console.log(5.7);
+console.log(Math.round(5.7));
+console.log(5.3);
+console.log(Math.round(5.3));
+console.log(-5.3);
+console.log(Math.round(-5.3));
---
+++ conditionals.js
@@ -0,0 +1,45 @@
+//Conditionals are if-statements
+
+let x=5;
+if (x==5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+
+let z = 5;
+if (z==5){
+    console.log("option1");
+}else {
+    console.log("option2")
+}
+console.log("end of program");
+
+let num2 = 6;
+if (num2==10){
+    console.log("equals 10");
+} else if (num2>5){
+    console.log("greater than 5");
+} else{
+    console.log("some number");
+}
+console.log("end of program");
+
+let secret = 70;
+if(secret % 7 == 0){
+    console.log("divisible by 7");
+}
+if (secret > 10){
+    console.log("greater than 10");
+} else{
+    console.log("not greater than 10");
+}
+console.log("end of program");
+
+let x = 16;
+if (x > 10) {
+    console.log("greater");
+}
+if (x > 5) {
+    console.log("not greater");
+}
+console.log("end of program")
