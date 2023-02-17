b071559ab92695e3f8c6dfcd5ec347adabb27d3d
ic59 <ic59@fresnou.org>
2023-02-17 10:27:57 -0800

    added while loops 1

---
+++ whileLoops1.js
@@ -0,0 +1,59 @@
+const readline = require(`readline-sync`)
+
+//1
+let x = 0;
+while (x<26){
+    console.log(x);
+    x++;
+}
+
+//2
+let y = 0;
+while (y<51){
+    console.log(y);
+    y++;
+}
+
+//3
+let w = -(10);
+while (w<11) {
+    console.log(w);
+    w++;
+}
+
+//4
+let s = 0;
+while (s>-51) {
+    console.log(s);
+    s--;
+}
+
+//5
+let p = 0;
+while (p<26){
+    console.log(p);
+    p+=2;
+}
+
+//6
+let r = 101;
+while (r>-1){
+    console.log(r);
+    r-=2;
+}
+
+//7
+let f = 0;
+while (f<99) {
+    console.log(f);
+    f+=3;
+}
+
+//8
+let a = 0;
+while (a<999){
+    console.log(a);
+    a+=5;
+
+
+
